c9ffe41bd6626514cf4c5348d17945d7229f4116
luppo1 <83582018+luppo1@users.noreply.github.com>
2021-11-11 04:12:20 +0000

    Update fasta2CSV.sh

--- fasta2CSV.sh
+++ fasta2CSV.sh
@@ -1,4 +1,4 @@
+#!/bin/bash
 sed '/^>/s/ .*//' 16s_sequences.fasta > 16s_sequences_1.fasta
 sed -e 's/\(^>.*$\)/#\1#/' 16s_sequences_1.fasta | tr -d "\r" | tr -d "\n" | sed -e 's/$/#/' | tr "#" "\n" | sed -e '/^$/d' > 16s_sequences_2.fasta
 ~/albert/fastx_bin/bin/./fasta_formatter -t -i 16s_sequences_2.fasta -o 16s_sequences_final.csv
-
